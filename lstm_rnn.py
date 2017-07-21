c72319c879c16dfe91afce9d7438781dee68b230
eddyrene <ddcharrez@gmail.com>
2017-07-21 10:01:08 -0500

    msre

--- lstm_rnn.py
+++ lstm_rnn.py
@@ -13,19 +13,39 @@ import preprocess_data as prep
 
 #################################################################
 ################## Formating dataset ############################
-df = prep.preprocess_aqp_dataset()
+def format_dataset():
+	df = prep.preprocess_aqp_dataset()
 
-dataset = df[df["date_time"].isin(pd.date_range('2006-12-31', \
+	dataset = df[df["date_time"].isin(pd.date_range('2006-12-31', \
 		'2017-06-30 23:00:00', freq = 'H'))]
 
-idx = pd.date_range('2006-12-31', '2017-06-30 23:00:00', freq='H')
+	idx = pd.date_range('2006-12-31', '2017-06-30 23:00:00', \
+				freq='H')
 
-dataset.set_index('date_time', inplace = True)
-dataset.index = pd.DatetimeIndex(dataset.index)
-dataset = dataset.reindex(idx, fill_value = np.nan)
+	dataset.set_index('date_time', inplace = True)
+	dataset.index = pd.DatetimeIndex(dataset.index)
+	dataset = dataset.reindex(idx, fill_value = np.nan)
 
-dataset = dataset.interpolate('linear')
-dataset.drop(['station_id'], axis = 1, inplace = True)
+	dataset = dataset.interpolate('linear')
+	dataset.drop(['station_id'], axis = 1, inplace = True)
+	return dataset	
+
+def get_unshuffled_dataset():
+	dataset = format_dataset()
+	# transform data to be supervised
+	ms = timeseries_to_supervised(dataset, 24)
+	print(len(ms))
+
+	# spliting data 20 % to test
+	p = 0.79994783516
+	temperatures = np.array(ms[['temperature', 'temp_r']])
+	train, test = temperatures[:int(len(temperatures)*p)], \
+			temperatures[int(len(temperatures)*p):]
+
+	scaler, train_scaled, test_scaled = scale(train, test)
+	return scaler, train_scaled, test_scaled
+
+
 
 ################################################################
 ##### frame a sequence as a supervised learning problem ########
@@ -54,12 +74,10 @@ def scale(train, test):
 	return scaler, train_scaled, test_scaled
  
 # inverse scaling for a forecasted value
-def invert_scale(scaler, X, value):
-	new_row = [x for x in X] + [value]
-	array = numpy.array(new_row)
-	array = array.reshape(1, len(array))
-	inverted = scaler.inverse_transform(array)
-	return inverted[0, -1]
+def invert_scale(scaler, X, y):
+	inverted_X = scaler.inverse_transform(X)
+	inverted_y = scaler.inverse_transform(y)
+	return inverted_X, inverted_y
 
 # fit an LSTM network to training data
 def fit_lstm(train, batch_size, nb_epoch, neurons):
@@ -79,26 +97,21 @@ def fit_lstm(train, batch_size, nb_epoch, neurons):
 
 ###################################################################
 
-# transfor data to be supervised
-ms = timeseries_to_supervised(dataset, 24)
-print(len(ms))
 
-# spliting data 20 % to test
-p = 0.79994783516
-temperatures = np.array(ms[['temperature', 'temp_r']])
-train, test = temperatures[:int(len(temperatures)*p)], \
-			temperatures[int(len(temperatures)*p):]
+if __name__ == "__main__":
 
-scaler, train_scaled, test_scaled = scale(train, test)
 
-#y = np.array(ms['temp_r'])
+	#y = np.array(ms['temp_r'])
 
-batch_size = 24
-n_epochs = 15
-neurons = 4
+	batch_size = 24
+	n_epochs = 15
+	neurons = 4
 
-lstm_model = fit_lstm(train_scaled, batch_size, n_epochs, neurons)
+	lstm_model = fit_lstm(train_scaled, batch_size,\
+			 n_epochs, neurons)
 
-lstm_model.save('models/lstm_24_1500.h5')
-lstm_model.save_weights('models/lstm_weights_24_1500.h5')
+	lstm_model.save('models/lstm_24_1500.h5')
+	lstm_model.save_weights('models/lstm_weights_24_1500.h5')
+
+	
 
--- weather_forecast.py
+++ weather_forecast.py
@@ -10,10 +10,52 @@ from keras.layers import Dense
 from keras.layers import LSTM
 from keras.models import load_model
 from math import sqrt
+import matplotlib
+matplotlib.use('Agg')
 from matplotlib import pyplot
 import numpy as np
+import lstm_rnn as ms
 
+lstm_model = load_model('models/lstm_24.h5')
+lstm_model.load_weights('models/lstm_weights_24.h5')
 
-model = load_model('models/lstm_24.h5')
-model_weights = model.load_weights('models/lstm_weights_24.h5')
+# one-step forecast
+def forecast_lstm(model, batch_size, X):
+	X = X.reshape(1, 1, len(X))
+	yhat = model.predict(X, batch_size=batch_size)
+	return yhat[0,0]
 
+def column(matrix, i):
+    return [row[i] for row in matrix]
+
+scaler, train_scaled, test_scaled = ms.get_unshuffled_dataset()
+
+train_reshaped = train_scaled[:, 0].reshape(len(train_scaled), 1, 1)
+
+lstm_model.predict(train_reshaped, batch_size=24)
+ 
+print(test_scaled)
+
+X, y = test_scaled[:, 0:-1], test_scaled[:, -1]
+X = X.reshape(X.shape[0], 1, X.shape[1])
+
+scores = lstm_model.evaluate(X, y, batch_size=24, verbose=1)
+print("\nmse:", scores)
+#print('mse=%f, mae=%f, mape=%f' % (scores[0],scores[1],scores[2]))
+
+predictions = lstm_model.predict(X, batch_size=24, verbose=1)
+
+print(predictions)
+print(len(predictions))
+print(len(y))
+	
+print(y[-24:])
+print(predictions[-24:])
+
+msra = [x[0] for x in predictions]
+print(msra[-24:])
+
+# line plot of observed vs predicted
+pyplot.plot(y[-24:])
+pyplot.plot(msra[-24:])
+pyplot.savefig("images/a.png")
